ea84d885a2985268470090ece67963403c0d40d4
Ben Coombes <benzcoombes@gmail.com>
2022-01-29 16:01:12 -0500

    enemies now rotate properly (no animation yet)

--- Assets/[Scripts]/EnemyMovement.cs
+++ Assets/[Scripts]/EnemyMovement.cs
@@ -7,18 +7,22 @@ public class EnemyMovement : MonoBehaviour
     public float movementSpeed = 5f;
     public float rotationSpeed = 5f;
 
+    private Quaternion lookRotation;
     private Transform target;
     private int currentWaypointIndex = 0;
 
     private void Start()
     {
         target = WaypointList.waypoints[0];
+        Vector3 dir = target.position - transform.position;
+        lookRotation = Quaternion.LookRotation(dir);
     }
 
     private void Update()
     {
         Vector3 direction = target.position - transform.position;
         transform.Translate(direction.normalized * movementSpeed * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
 
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance < 0.05)
@@ -33,11 +37,7 @@ public class EnemyMovement : MonoBehaviour
         currentWaypointIndex++;
         target = WaypointList.waypoints[currentWaypointIndex];
 
-
-
         Vector3 dir = target.position - transform.position;
-        Quaternion lookRotation = Quaternion.LookRotation(dir);
-        Vector3 rotation = lookRotation.eulerAngles;
-        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        lookRotation = Quaternion.LookRotation(dir);
     }
 }
