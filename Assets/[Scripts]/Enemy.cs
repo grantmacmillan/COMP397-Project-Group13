d31f027394a0283e288e201ba9def41ec087c03b
Ben Coombes <benzcoombes@gmail.com>
2022-01-29 16:57:05 -0500

    renaming of enemymovement script to enemy

--- Assets/[Scripts]/Enemy.cs
+++ Assets/[Scripts]/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public float rotationSpeed = 5f;
