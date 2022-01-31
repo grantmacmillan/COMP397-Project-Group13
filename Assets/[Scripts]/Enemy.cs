043cef18b98bdc92b7d744278a7ad536fd9250e2
Tatsiana <tptushko@my.centennialcollege.ca>
2022-01-30 21:35:05 -0500

    Added anim states in Enemy script

--- Assets/[Scripts]/Enemy.cs
+++ Assets/[Scripts]/Enemy.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private enum State
+    {
+        Idle,
+        Walk,
+        Run,
+        Dead
+    }
+
     public float movementSpeed = 5f;
     public float rotationSpeed = 5f;
 
+    private State state;
+    private Animator animator;
     private Quaternion lookRotation;
     private Transform target;
     private int currentWaypointIndex = 0;
 
     private void Start()
     {
+        state = State.Walk;
+        animator = GetComponent<Animator>();
         target = WaypointList.waypoints[0];
         Vector3 dir = target.position - transform.position;
         lookRotation = Quaternion.LookRotation(dir);
@@ -20,6 +32,23 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
+        switch (state)
+        {
+            case State.Idle:
+                animator.SetFloat("movementSpeed", 0f);
+                break;
+            case State.Walk:
+                animator.SetFloat("movementSpeed", movementSpeed);
+                break;
+            case State.Run:
+                animator.SetFloat("movementSpeed", movementSpeed*2);
+                break;
+            case State.Dead:
+                animator.SetBool("isDead", true);
+                break;
+
+        } 
+
         Vector3 direction = target.position - transform.position;
         transform.Translate(direction.normalized * movementSpeed * Time.deltaTime, Space.World);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
