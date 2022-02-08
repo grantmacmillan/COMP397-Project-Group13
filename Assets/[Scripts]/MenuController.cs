34b63e5645acff258d497e9dd26d0907be55c8d9
Nandini <juicyfruit5600@yahoo.ca>
2022-02-08 18:13:25 -0500

    Added Menu Select Audio

--- Assets/[Scripts]/MenuController.cs
+++ Assets/[Scripts]/MenuController.cs
@@ -6,20 +6,15 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void update()
     {
-        
+        FindObjectOfType<Sound_Manager>().Play("MenuSelect");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void NewGame()
     {
+        
         SceneManager.LoadScene(1);
+        
     }
     public void QuitGame()
     {
