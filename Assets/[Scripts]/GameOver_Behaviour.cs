333d82e00ce4e3e9bfa82d79282503ffb1c7a959
Nandini <juicyfruit5600@yahoo.ca>
2022-02-08 17:19:29 -0500

    Game Over Screen Functional

---
+++ Assets/[Scripts]/GameOver_Behaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver_Behaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
